88f5a15ac1c6d5a183fb4a07c826c17df9e48180
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-06-06 13:00:34 -0700

    commit lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+var multiply = num1 * num2;
 
+const random = Math.floor (Math.random() *10);
+
+const num3 = 12
+const num4 = 3
+var mod = num3/num4;
+
+const max = Math.max(2, 12, 6, 20)
 // Write your code here
